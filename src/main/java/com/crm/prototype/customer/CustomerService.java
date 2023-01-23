bd4da2b4d262974180be4e64be8c5f33296d4f07
Ayush Kaushik <anon.programmer305@gmail.com>
2023-01-22 22:29:16 -0500

    Finished update workflow and added CICD
    - Finished update customer workflow
    - First iteration of github actions

--- src/main/java/com/crm/prototype/customer/CustomerService.java
+++ src/main/java/com/crm/prototype/customer/CustomerService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,6 +32,16 @@ public class CustomerService {
     }
 
     public void updateCustomer(CustomerUpdateDTO customerUpdateDTO) {
+        Optional<CustomerEntity> optionalCustomerEntity = customerRepository.findById(customerUpdateDTO.getId());
+        CustomerEntity customerEntity = optionalCustomerEntity.get();
 
+        customerEntity.setFirstName(customerUpdateDTO.getFirstName());
+        customerEntity.setLastName(customerUpdateDTO.getLastName());
+        customerEntity.setPhoneNumber(customerUpdateDTO.getPhoneNumber());
+        customerEntity.setAddress(customerUpdateDTO.getAddress());
+        customerEntity.setCountry(customerUpdateDTO.getCountry());
+        customerEntity.setEmail(customerUpdateDTO.getEmail());
+
+        customerRepository.save(customerEntity);
     }
 }
