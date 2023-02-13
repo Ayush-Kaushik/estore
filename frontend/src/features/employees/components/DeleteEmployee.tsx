987777c8125451ac0b4094c02c9e5044587b7bd7
Ayush Kaushik <anon.programmer305@gmail.com>
2023-02-12 23:16:47 -0500

    Setup react frontend

---
+++ frontend/src/features/employees/components/DeleteEmployee.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const DeleteEmployee = () => {
+  return (
+    <div>
+    </div>
+  )
+};
+
+export default DeleteEmployee;
---
+++ frontend/src/features/employees/components/EmployeesList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const EmployeesList = () => {
+  return (
+    <div>
+    </div>
+  )
+};
+
+export default EmployeesList;
